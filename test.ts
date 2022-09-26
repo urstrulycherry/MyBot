27b7a31be9c97f6e4d10c29957d31b90f4883292
urstrulycherry <gcharannetha@gmail.com>
2022-09-26 16:07:19 +0530

    removed unused files

--- test.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-var fs = require('fs');
-//get all files in modules folder
-var files = fs.readdirSync('../');
-// print all and filter out all files
-files.forEach(function (file: any) {
-    console.log(file);
-})
