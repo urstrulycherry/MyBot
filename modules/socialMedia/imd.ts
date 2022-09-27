012148bc3b7de4d4d8a53733865c6bbd967c1d28
Sairam <sairam21240306@gmail.com>
2022-09-27 16:17:22 +0530

    Combined imd and tmd into md as single module and added tsconfig

---
+++ modules/socialMedia/imd.ts
@@ -0,0 +1,16 @@
+import WAWebJS from "whatsapp-web.js";
+import { send } from "../../util/reply";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const instagramGetUrl = require("instagram-url-direct");
+const noMedia = "No media found";
+
+export const imd = async (message: WAWebJS.Message, url: string) => {
+    const response = await instagramGetUrl(url);
+    if (response.results_number > 0) {
+        response.url_list.forEach((mediaUrl: string) => {
+            send.url(message, mediaUrl);
+        });
+    } else {
+        send.text(message, noMedia);
+    }
+};
