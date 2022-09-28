13c9372302096e8ad19b026ff2f4b37b02856d4d
Rajkumar_Telukuntla <rajkumar_telukuntla@epam.com>
2022-09-28 14:05:37 +0530

    Added Author name to the quote module

--- modules/quote.ts
+++ modules/quote.ts
@@ -1,7 +1,8 @@
 import WAWebJS from "whatsapp-web.js";
 import puppeteer from "puppeteer";
 
-const Xpath = '//*[@id="mf-qotd"]/div/div[2]/table/tbody/tr[1]/td/table/tbody/tr/td[3]/table/tbody/tr[1]/td';
+const quotePath = '//*[@id="mf-qotd"]/div/div[2]/table/tbody/tr[1]/td/table/tbody/tr/td[3]/table/tbody/tr[1]/td';
+const authorPath = '//*[@id="mf-qotd"]/div/div[2]/table/tbody/tr[1]/td/table/tbody/tr/td[3]/table/tbody/tr[2]/td';
 
 const process = async (message: WAWebJS.Message, _client: WAWebJS.Client) => {
     console.log("quote");
@@ -12,10 +13,14 @@ const trigger = async (message:WAWebJS.Message) => {
     const browser = await puppeteer.launch({});
     const page = await browser.newPage();
     await page.goto("https://en.wikiquote.org/wiki/Main_Page");
-    await page.waitForXPath(Xpath);
-    const [element] = await page.$x(Xpath);
-    const text = await page.evaluate(ele => ele.textContent, element);
-    const result = `*${new Date().toDateString()}*\n${text?.trim()}\n_Have a Good Day!_`;
+    await page.waitForXPath(quotePath);
+    const [element1] = await page.$x(quotePath);
+    const quoteText = await page.evaluate(ele => ele.textContent, element1);
+    const [element2] = await page.$x(authorPath);
+    const authorName =  await page.evaluate(ele => ele.textContent, element2);
+    const emoji1 = "😊❤️️🌞";
+    const emoji2 = "☀️☕➡️️😋";
+    const result = `*${new Date().toDateString()}* ${emoji1}\n${quoteText?.trim()}\n_${authorName?.substring(1, authorName.length - 2).trim()}_\n_Have a Good Day!_ ${emoji2}`;
     browser.close();
     message.reply(result);
 };
