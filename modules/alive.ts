97076a14839c51f92eae95489aa8cf62b3392051
urstrulycherry <gcharannetha@gmail.com>
2022-09-29 13:11:18 +0530

    list fixed

--- modules/alive.ts
+++ modules/alive.ts
@@ -2,11 +2,8 @@ import WAWebJS from "whatsapp-web.js";
 import { send } from "../util/reply";
 
 const process = async (message: WAWebJS.Message, _client: WAWebJS.Client) => {
-    console.log ("alive");
+    console.log("alive");
     send.text(message, "Huh and you..?");
-
-
-
 };
 
 module.exports = {
