5c8064d75bf08e650a4935c9661f46d7b6611ce8
urstrulycherry <gcharannetha@gmail.com>
2022-09-30 18:54:47 +0530

    Updated search.ts

--- modules/search.ts
+++ modules/search.ts
@@ -2,9 +2,9 @@ import puppeteer from "puppeteer";
 import WAWebJS from "whatsapp-web.js";
 import { send } from "../util/reply";
 
+const error = "Sorry, I couldn't find any images for that search.";
 const process = async (message: WAWebJS.Message) => {
     console.log("Image Search");
-    const error = "Sorry, I couldn't find any images for that search.";
     try {
         const search = message.body.slice(8).split(" ").join("+");
         if (!(search.split("+").length > 0)) return;
@@ -15,15 +15,19 @@ const process = async (message: WAWebJS.Message) => {
 };
 
 const trigger = async (message: WAWebJS.Message, search: string) => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(`https://duckduckgo.com/?va=j&t=hb&q=${search}&iax=images&ia=images`);
-    const selector = "#zci-images > div > div.tile-wrap > div > div:nth-child(3) > div.tile--img__media > span > img";
-    await page.waitForSelector(selector);
-    const src = await page.$eval(selector, (img) => img.getAttribute("src"));
-    if (!src) return;
-    browser.close();
-    send.url(message, `https://${src.slice(2)}`);
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`https://duckduckgo.com/?va=j&t=hb&q=${search}&iax=images&ia=images`);
+        const selector = "#zci-images > div > div.tile-wrap > div > div:nth-child(3) > div.tile--img__media > span > img";
+        await page.waitForSelector(selector);
+        const src = await page.$eval(selector, (img) => img.getAttribute("src"));
+        if (!src) return;
+        browser.close();
+        send.url(message, `https://${src.slice(2)}`);
+    } catch (_) {
+        send.text(message, error);
+    }
 };
 
 module.exports = {
