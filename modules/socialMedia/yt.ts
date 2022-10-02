cf8726604b5d65b152918bc7b33b9fa9d0f4d328
urstrulycherry <gcharannetha@gmail.com>
2022-10-02 15:47:21 +0530

    video <70sec and changed to tinyurl

--- modules/socialMedia/yt.ts
+++ modules/socialMedia/yt.ts
@@ -16,16 +16,25 @@ export const yt = async (message: WAWebJS.Message, url: string) => {
         await page.waitForSelector(selector);
         const yourHref = await page.$eval(selector, anchor => anchor.getAttribute("href"));
         if (!yourHref) return;
-        await page.goto("https://www.shorturl.at");
-        selector = "#formurl > input[type = text]";
+        selector = "#sf_result > div > div.result-box.video > div.info-box > div.meta > div.row.duration";
+        await page.waitForSelector(selector);
+        const duration = await page.$eval(selector, div => div.textContent);
+        if (!duration) return;
+        const seconds = duration.split(":").reverse().reduce((prev, curr, i) => prev + parseInt(curr, 10,) * 60 ** i, 0);
+        if (seconds < 70) {
+            return send.url(message, yourHref);
+        }
+        page.goto("https://tinyurl.com/app/");
+        selector = "#long-url";
         await page.waitForSelector(selector);
         await page.type(selector, yourHref);
-        await page.click("#formbutton > input[type=submit]");
-        selector = "#shortenurl";
+        selector = "#tinyurl > div.root > div.d-flex.flex-column.overflow-y-a.h-100 > section > div > div > div:nth-child(1) > div > div.col-lg-8 > div > div.col-md-7 > div > div > div > div > form > div.d-flex.align-items-center.mt-3 > button.btn.btn-block.btn-t-green.btn-xl.d-md-none";
+        await page.keyboard.press("Enter");
+        selector = "#tinyurl > div.root > section > div > div.flex-grow-1.overflow-y-a.border-bottom > div.border-top > div > div > div > div > div.flex-fill > div.alias > div";
         await page.waitForSelector(selector);
-        const link = await page.$eval(selector, anchor => anchor.getAttribute("value"));
-        browser.close();
-        send.text(message, `https://${link}`);
+        let link = await page.$eval(selector, div => div.textContent);
+        link = `https://${link?.trim()}`;
+        send.text(message, link);
     } catch (_) {
         send.text(message, error);
     }
