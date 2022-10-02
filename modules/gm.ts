d4071e2ecd3ce5cdf6311b714e48cd2c89f44a79
urstrulycherry <gcharannetha@gmail.com>
2022-10-02 13:03:57 +0530

    removed unnecessary console logs

--- modules/gm.ts
+++ modules/gm.ts
@@ -8,20 +8,16 @@ const process = async (message: WAWebJS.Message, _client: WAWebJS.Client) => {
 
     const chat = await message.getChat();
     await chat.fetchMessages({ limit: 500 });
-    console.log("before get quoted message");
     const quotedMsg = await message.getQuotedMessage();
-    console.log("after get quoted message");
     if (!quotedMsg) {
         send.text(message, noMedia);
         return;
     }
     if (quotedMsg.hasMedia) {
-        console.log("before download media");
         const media = await quotedMsg.downloadMedia().catch(() => {
             send.text(message, noMedia);
             return;
         });
-        console.log("after download media");
         if (!media) return;
         send.media(message, media);
     } else {
