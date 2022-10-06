8910615a5e9000eeb109bc9b8af32cd731b3dc73
urstrulycherry <gcharannetha@gmail.com>
2022-10-06 20:27:42 +0530

    Fixed twitter download bug

--- modules/socialMedia/tmd.ts
+++ modules/socialMedia/tmd.ts
@@ -6,33 +6,37 @@ import { send } from "../../util/reply";
 const noMedia = "No media found";
 
 export const tmd = async (message: WAWebJS.Message, url: string) => {
-    const urls = [];
-    const { id } = parseUrl(url);
-    const details = getDetails(id);
-    for (let i = 0; i < details.globalObjects.tweets[id].extended_entities.media.length; i++) {
-        if (details.globalObjects.tweets[id].extended_entities.media[i].video_info) {
-            let maxWidth = -1;
-            let maxWidthIndex = -1;
-            for (let j = 0; j < details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants.length; j++) {
-                if (details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants[j].content_type === "video/mp4") {
-                    if (Number(details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants[j].bitrate) >= maxWidth) {
-                        maxWidth = Number(details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants[j].bitrate);
-                        maxWidthIndex = j;
+    try {
+        const urls = [];
+        const { id } = parseUrl(url);
+        const details = getDetails(id);
+        for (let i = 0; i < details.globalObjects.tweets[id].extended_entities.media.length; i++) {
+            if (details.globalObjects.tweets[id].extended_entities.media[i].video_info) {
+                let maxWidth = -1;
+                let maxWidthIndex = -1;
+                for (let j = 0; j < details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants.length; j++) {
+                    if (details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants[j].content_type === "video/mp4") {
+                        if (Number(details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants[j].bitrate) >= maxWidth) {
+                            maxWidth = Number(details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants[j].bitrate);
+                            maxWidthIndex = j;
+                        }
                     }
                 }
+                if (maxWidthIndex !== -1) {
+                    urls.push(details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants[maxWidthIndex].url);
+                }
+            } else {
+                urls.push(details.globalObjects.tweets[id].extended_entities.media[i].media_url_https);
             }
-            if (maxWidthIndex !== -1) {
-                urls.push(details.globalObjects.tweets[id].extended_entities.media[i].video_info.variants[maxWidthIndex].url);
-            }
-        } else {
-            urls.push(details.globalObjects.tweets[id].extended_entities.media[i].media_url_https);
         }
-    }
-    if (urls.length === 0) {
-        return send.text(message, noMedia);
-    }
-    for (let i = 0; i < urls.length; i++) {
-        if (!urls[i]) continue;
-        await send.url(message, urls[i]);
+        if (urls.length === 0) {
+            return send.text(message, noMedia);
+        }
+        for (let i = 0; i < urls.length; i++) {
+            if (!urls[i]) continue;
+            await send.url(message, urls[i]);
+        }
+    } catch (e) {
+        send.text(message, "Something went wrong");
     }
 };
