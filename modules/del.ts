cdfb5f753382b292091751eb2029f84c7cebee2a
urstrulycherry <gcharannetha@gmail.com>
2022-11-27 18:23:01 +0530

    Added delete module

---
+++ modules/del.ts
@@ -0,0 +1,43 @@
+/* eslint-disable */
+import WAWebJS from "whatsapp-web.js";
+import { helper } from "../util/helper";
+import { react, send } from "../util/reply";
+
+const process = async (message: WAWebJS.Message, client: WAWebJS.Client, options: WAWebJS.MessageSendOptions) => {
+    console.log("delete");
+    if (!message.hasQuotedMsg) return react.warning(message);
+    const chat = await message.getChat();
+    if (!chat.isGroup && !message.fromMe) return react.warning(message);
+    await chat.fetchMessages({ limit: 500 });
+    const quotedMsg = await message.getQuotedMessage();
+    if (!chat.isGroup && !quotedMsg.fromMe) return react.warning(message);
+    const res = await trigger(quotedMsg, client);
+    if (!res) {
+        return react.warning(message);
+    }
+    react.success(message);
+    if (!message.body.includes("-s")) return;
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    await trigger(message, client);
+}
+const trigger = async (message: WAWebJS.Message, client: WAWebJS.Client) => {
+    const chat = await message.getChat();
+    if (chat.isGroup) {
+        if (await helper.isAdmin(message, client)) {
+            await message.delete(true);
+            return true;
+        } else {
+            return false;
+        }
+    } else if (message.fromMe) {
+        await message.delete(true);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+module.exports = {
+    name: "del",
+    process
+};
