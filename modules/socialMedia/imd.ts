508747b7e5aa4bd57c5991c8ded8e4b15c9e63eb
urstrulycherry <gcharannetha@gmail.com>
2023-06-26 15:37:40 +0530

    updated imd

--- modules/socialMedia/imd.ts
+++ modules/socialMedia/imd.ts
@@ -1,21 +1,13 @@
 import WAWebJS from "whatsapp-web.js";
 import { Send } from "../../util/reply";
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const instagramGetUrl = require("instagram-url-direct");
+import { instaSave } from "./imd_helper";
 const noMedia = "No media found";
 const error = "Something went wrong, please try again later";
 export const imd = async (message: WAWebJS.Message, options: WAWebJS.MessageSendOptions, url: string) => {
-    type igRes = {
-        results_number: number;
-        url_list: string[];
-    }
-    instagramGetUrl(url).then((res: igRes) => {
-        if (res.results_number > 0) {
-            res.url_list.forEach((mediaUrl: string) => {
-                if (mediaUrl.includes("https://media.instasupersave.com")) {
-                    mediaUrl = decodeURIComponent(mediaUrl).split("uri=")[1].split("&filename=")[0];
-                }
-                Send.url(message, options, mediaUrl);
+    instaSave(url).then(async (res: any) => {
+        if (res.status) {
+            res.data.forEach(async (v: any) => {
+                Send.url(message, options, v);
             });
         } else {
             Send.catch(message, noMedia);
