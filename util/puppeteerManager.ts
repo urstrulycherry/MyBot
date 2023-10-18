fffa94588ea77014b6912f41d88041eac74bb62e
urstrulycherry <gcharannetha@gmail.com>
2023-10-18 10:35:43 +0530

    added Puppeteer Manager

---
+++ util/puppeteerManager.ts
@@ -0,0 +1,30 @@
+import puppeteer, { Browser, Page } from "puppeteer";
+
+let browser: Browser | null = null;
+
+async function initializeBrowser() {
+    browser = await puppeteer.launch({
+        headless: false,
+        args: [
+            "--no-sandbox"
+        ]
+    });
+}
+
+export async function newPage(): Promise<Page> {
+    if (!browser) {
+        await initializeBrowser();
+    }
+    if (browser) {
+        const page = await browser.newPage();
+        return page;
+    }
+    throw new Error("Browser is not initialized");
+}
+
+export async function closeBrowser() {
+    if (browser) {
+        await browser.close();
+        browser = null;
+    }
+}
