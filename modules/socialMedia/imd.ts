211604d6bc9c6556b3d1870577a214f71c39f8be
urstrulycherry <gcharannetha@gmail.com>
2024-04-06 22:03:05 +0530

    removed existing code

--- modules/socialMedia/imd.ts
+++ modules/socialMedia/imd.ts
@@ -1,19 +1,6 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import WAWebJS from "whatsapp-web.js";
 import { Send } from "../../util/reply";
-import { instaSave } from "./imd_helper";
-const noMedia = "No media found";
-const error = "Something went wrong, please try again later";
-export const imd = async (message: WAWebJS.Message, options: WAWebJS.MessageSendOptions, url: string) => {
-    instaSave(url).then(async (res: any) => {
-        if (res.status) {
-            res.data.forEach(async (v: any) => {
-                Send.url(message, options, v);
-            });
-        } else {
-            Send.catch(message, noMedia);
-        }
-    }).catch(() => {
-        Send.catch(message, error);
-    });
+
+export const imd = async (message: WAWebJS.Message, options: WAWebJS.MessageSendOptions, _url: string) => {
+    Send.text(message, options, "Coming soon...");
 };
--- modules/socialMedia/tmd.ts
+++ modules/socialMedia/tmd.ts
@@ -1,37 +1,6 @@
 import WAWebJS from "whatsapp-web.js";
 import { Send } from "../../util/reply";
-import * as dotenv from "dotenv";
-import { newPage } from "../../util/puppeteerManager";
 
-dotenv.config();
-
-const noMedia = "No media found";
-const invalid = "Invalid url/Something went wrong";
-
-export const tmd = async (message: WAWebJS.Message, options: WAWebJS.MessageSendOptions, url: string) => {
-    try {
-        const username = new URL(url).pathname.split("/")[1];
-        const tweetId = new URL(url).pathname.split("/")[3];
-        const page = await newPage();
-        await page.goto(url);
-        await page.waitForSelector("img", { visible: true });
-        const imageNodes = await page.$x(`//a[contains(@href, "/${username}/status/${tweetId}/photo/")]`);
-        const imageUrls = await Promise.all(
-            imageNodes.map(async (node) => {
-                const imgTag = await node.$("img");
-                const imageUrl = await imgTag?.evaluate((img) => img.getAttribute("src"));
-                return `${imageUrl?.split("?")[0]}?format=jpg&name=4096x4096`;
-            })
-        );
-        await page.close();
-        if (imageUrls.length > 0) {
-            imageUrls.forEach((imageUrl) => {
-                Send.url(message, options, imageUrl);
-            });
-        } else {
-            Send.catch(message, noMedia);
-        }
-    } catch (error) {
-        Send.catch(message, invalid);
-    }
+export const tmd = async (message: WAWebJS.Message, options: WAWebJS.MessageSendOptions, _url: string) => {
+    Send.text(message, options, "Coming soon...");
 };
